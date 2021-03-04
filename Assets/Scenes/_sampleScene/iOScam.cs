9ff94d873f2fe419842159b5a3fd083b3de5e912
yash1shi <78535052+yash1shi@users.noreply.github.com>
2021-03-04 11:45:40 +0900

    Add files via upload

---
+++ Assets/Scenes/_sampleScene/iOScam.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.iOS;
+using System;
+
+
+
+public class iOScam : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        findWebCams();
+
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            Debug.Log("webcam found");
+        }
+        else
+        {
+            Debug.Log("webcam not found");
+        }
+
+        findMicrophones();
+
+        yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+        if (Application.HasUserAuthorization(UserAuthorization.Microphone))
+        {
+            Debug.Log("Microphone found");
+        }
+        else
+        {
+            Debug.Log("Microphone not found");
+        }
+    }
+
+    void findWebCams()
+    {
+        foreach (var device in WebCamTexture.devices)
+        {
+            Debug.Log("Name: " + device.name);
+        }
+    }
+
+    void findMicrophones()
+    {
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+    }
+}
